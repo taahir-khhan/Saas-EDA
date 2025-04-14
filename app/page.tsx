a2e92b09f40ff2ba970617b850dbfdfa43caa7f1
Tahir Khan <tahirtrk42@gmail.com>
2025-04-14 13:39:10 +0530

    databse model creation and synching the prisma schema with the database

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='w-full min-h-screen flex items-center justify-center bg-gray-600 text-white'>
+    <div className='w-full min-h-screen flex items-center justify-center bg-gray-600 text-white text-5xl'>
       Hello Next.js
     </div>
   );
